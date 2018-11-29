c2fbd235b79231d795527d17e91bc3ffcffd89b9
daryal89 <creativity80@gmail.com>
2018-11-29 13:03:15 -0500

    2nd file added

---
+++ src/main/java/testRepository/TestDemo2.java
@@ -0,0 +1,14 @@
+package testRepository;
+
+public class TestDemo2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int sum = a + b + c;
+		System.out.println("The sum is: " + sum);
+
+	}
+
+}
